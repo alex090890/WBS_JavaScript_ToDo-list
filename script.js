0a9e5f6b2074536367eb59b61614741e7e3b7cda
OLEKSANDR LAZURENKO <alexprofteach@gmail.com>
2023-12-16 13:19:05 +0000

    Worked on styling

--- script.js
+++ script.js
@@ -1,22 +1,24 @@
+//Create the variable connected with the HTML
 const addNewTask = document.querySelector(".addnewtask");
 const addNewTaskForm = document.querySelector(".addnewtaskform");
 const cancelAdding = document.querySelector("#cancelAdding");
 
-
+//Open the input field menu when clicked on the "Add a new task button"
 let open = function() {
     addNewTaskForm.style.display = "block";
     addNewTask.style.display = "none";
 };
-
 addNewTask.addEventListener('click', open);
 
+//Close the menu when clicking the "Cancel button"
 let close = function() {
     addNewTaskForm.style.display = "none";
     addNewTask.style.display = "block";
 };
-
 cancelAdding.addEventListener('click', close);
 
+
+//Add a new list item
 document.getElementById('myForm').addEventListener('submit', function(event) {
     event.preventDefault(); // Prevents form submission and page reload
 
